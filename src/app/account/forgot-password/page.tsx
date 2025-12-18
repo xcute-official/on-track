6ae1a798395c709023133d446cd1650b5cf7ff4c
popsy-pops <popsy.pops01@outlook.com>
2025-12-18 11:12:42 -0500

    remove testing code from this page

--- src/app/account/forgot-password/page.tsx
+++ src/app/account/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import CalendarReport from "@/components/calendar-report";
 const page = () => {
   return (
     <div className="md:w-1/2 mx-auto mt-24 flex flex-col gap-4 justify-center px-6 py-6">
-      <CalendarReport />
+      
     </div>
   );
 };
